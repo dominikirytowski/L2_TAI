aa9aea843ba433016bf390a1d5a16abd2b88083b
Dominik <kamyka100@gmail.com>
2021-03-15 08:14:06 +0100

    init

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+
+
+let nav = '<nav><ul>'+
+	'<li><a href="index.html">Strona główna</a></li>\n\n'+
+	'<li><a href="contact.html">Kontakt</a></li></ul></nav>\n';
+let navbarElem = document.querySelector(".navbar");
+navbarElem.innerHTML = nav;
+
+
